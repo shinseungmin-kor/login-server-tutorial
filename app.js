78e59f6df03aa2519b07ef705d94aec57a84e2a5
shinseungmin-kor <shinseungmin10@gmail.com>
2021-08-28 01:05:37 +0900

    21.08.28 ejs 실습

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+// 앱세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home/index");
+});
+
+app.get("/login", (req, res) => {
+    res.render("home/login");
+});
+
+app.listen(3000, () => {
+    console.log("서버 가동");
+});
