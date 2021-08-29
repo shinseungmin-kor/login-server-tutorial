fb0c6155f49c9be02e49b7410e646a930e905c68
shinseungmin-kor <shinseungmin10@gmail.com>
2021-08-30 01:51:28 +0900

    21.08.30

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 const home = require("./routes/home");
-const PORT = 3000; 
+
 
 // 앱세팅
 app.set("views", "./views");
@@ -11,6 +11,5 @@ app.set("view engine", "ejs");
 
 app.use("/", home); // use-> 미들웨어를 등록하는 메서드
 
-app.listen(PORT, () => {
-    console.log("서버 가동");
-});
+module.exports = app;
+
