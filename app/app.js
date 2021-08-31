3614eecee38ed3f7745306124208447a5b89954b
shinseungmin-kor <shinseungmin10@gmail.com>
2021-09-01 02:05:31 +0900

    js파일 생성

--- app/app.js
+++ app/app.js
@@ -1,13 +1,17 @@
 "use srtict";
 
+// 모듈
 const express = require("express");
 const app = express();
+
+// 라우팅
 const home = require("./src/routes/home");
 
 
 // 앱세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use-> 미들웨어를 등록하는 메서드
 
