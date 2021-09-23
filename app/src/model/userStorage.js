a4cf97d110cc558e7be6b03eac40f7c82905c9cd
shinseungmin-kor <shinseungmin10@gmail.com>
2021-09-23 23:09:55 +0900

    21.09.23

--- app/src/model/userStorage.js
+++ app/src/model/userStorage.js
@@ -13,9 +13,10 @@ class userStorage {
             }, {});
             return userInfo; 
     }
-    
-    static getUsers(...fields) {
-        // const users = this.#users;
+
+    static #getUsers(data, isAll, fields) {
+        const users = JSON.parse(data);
+        if(isAll) return users
         const newUsers = fields.reduce((newUsers, field) => {
             if(users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -24,6 +25,16 @@ class userStorage {
         }, {});
         // console.log(newUsers);
         return newUsers;
+    }
+    
+    static getUsers(isAll, ...fields) {
+        return  fs.readFile("./src/databases/users.json")
+         .then((data) => {
+            return this.#getUsers(data, isAll, fields);
+         })
+         .catch(console.error);
+        // const users = this.#users;
+       
     }
     static getUserInfo(id) {
         // const users = this.#users;
@@ -36,14 +47,18 @@ class userStorage {
 
     
 
-    static save(userInfo) {
-        // const users = this.#users;
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.psword.push(userInfo.psword);
-        return { success: true };
-
+    static async save(userInfo) {
+        const users = await this.getUsers(true);
+        if(users.id.includes(userInfo.id)) {
+            throw "이미 존재하는 아이디 입니다.";
+        }
+            users.id.push(userInfo.id);
+            users.psword.push(userInfo.psword);
+            users.name.push(userInfo.name);
+            fs.writeFile("./src/databases/users.json", JSON.stringify(users))
+            return { success: true };
+        }
     }
-}
+
 
 module.exports = userStorage;
